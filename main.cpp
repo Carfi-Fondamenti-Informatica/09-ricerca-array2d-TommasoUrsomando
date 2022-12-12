3f028eb50d800a35018333f031cd5b965b0d370f
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-12-12 17:53:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "lib.h"
+int main() {
+    char a[10][20];
+    char b[20];
+    for(int i=0; i<10; i++){
 
-int main(){
-  return 0;
+        for(int j=0; j<20; j++){
+            a[i][j]=' ';
+            b[j]=' ';
+        }
+    }
+    for(int i=0; i<10; i++){
+        cin >> a[i];
+    }
+    cin >> b;
+
+    int p=0;
+    if(funzione(a, b, p)){
+        cout<<p;
+    }
+    else
+        cout<<"non presente";
+
+    return 0;
 }
